e59c83e0f6f7dacf9c52a3b6d59f5b131ad6a773
balbeeraswal <itcell-swd-uk@nic.in>
2026-04-18 21:10:24 +0530

    sdfdsfs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,5 +13,11 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+builder.Services.AddSingleton<ServiceBusClient>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var conn = config["ServiceBusConnectionString"]; // must exist in local.settings.json
+    return new ServiceBusClient(conn);
+});
 
 builder.Build().Run();
